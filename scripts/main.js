205a5b0570315b37f4a68987d6775a108aa38542
diegothomas2025 <diegothomas4@hotmail.com>
2025-09-09 21:52:18 -0300

    if and switch added

--- scripts/main.js
+++ scripts/main.js
@@ -9,5 +9,39 @@
       });
     });
 
+const hour = new Date().getHours();
+let welcomeMessage = document.querySelector(".welcomemessage");
+
+// IF
+
+// if (hour < 12) {
+//   welcomeMessage.textContent = "¡Buenos días! Es un gran horario para estudiar ¡Sigue así!"
+// } else if (hour < 18) {
+//   welcomeMessage.textContent = "¡Buenas tardes! Es un gran horario para estudiar ¡No olvides merendar!"
+// } else {
+//   welcomeMessage.textContent = "¡Buenas noches! Es un gran horario para estudiar ¡No olvides descansar!"
+// }
  
+//SWITCH
+let timeOfDay;
+
+if (hour < 12) {
+  timeOfDay = "morning";
+} else if (hour < 18) {
+  timeOfDay = "afternoon";
+} else {
+  timeOfDay = "night";
+}
+
+switch (timeOfDay) {
+  case "morning":
+    welcomeMessage.textContent = "¡Buenos días! Es un gran horario para estudiar ¡Sigue así!";
+    break;
+  case "afternoon":
+    welcomeMessage.textContent = "¡Buenas tardes! Es un gran horario para estudiar ¡No olvides merendar!";
+    break;
+  case "night":
+    welcomeMessage.textContent = "¡Buenas noches! Es un gran horario para estudiar ¡No olvides descansar!";
+    break;
+}
   
